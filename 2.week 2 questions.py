a9c92d6ecb6a60f58819918897eb833df73e3064
Yunus <101994700+mogelijkheid@users.noreply.github.com>
2022-04-13 10:57:22 +0200

    2.week 2. question

---
+++ 2.week 2 questions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+data = { 'a': [5, 3, 9, 0, 2, 3, 1], 'b': [4, 7, 5, 1, 2], 'c': [8, 1, 3, 2, 4] }
+for i in data.values():
+    i.sort()
+print(data)
+
+
+# In[ ]:
+
+
+
+
